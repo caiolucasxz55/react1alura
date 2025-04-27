d75512f2e6840e53317475216ca1d0079686f4b0
joaoGFG <joaogabrielfg10@gmail.com>
2025-04-27 19:25:41 -0300

    refactor: automatização da criação de paginas, componente de texto principal

--- src/app/listagem/denuncias/page.tsx
+++ src/app/listagem/denuncias/page.tsx
@@ -1,18 +1,18 @@
 // src/app/listagem/denuncias/page.tsx
 
 import InfoCard from "@/components/InfoCard/InfoCard";
+import TextoPagina from "@/components/TextoPagina/TextoPagina";
 
 export default function DenunciasPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col p-6">
-
       <div className="flex-1 flex flex-col items-center justify-center">
-        <h1 className="text-4xl md:text-5xl font-extrabold text-red-600 mb-2">
-          Denúncias e Reclamações
-        </h1>
-        <p className="text-gray-700 text-lg md:text-xl">
-          Canal de atendimento para receber denúncias e reclamações de pessoas com deficiência (PCD).
-        </p>
+          <TextoPagina 
+            title="Denúncias e Reclamações" 
+            colorTitle="text-red-600"
+            description="Canal de atendimento para receber denúncias e reclamações de pessoas com deficiência (PCD)." 
+            colorDescription="text-gray-700"
+          />
       </div>
 
       <div className="flex-1 flex items-center justify-center px-4">
